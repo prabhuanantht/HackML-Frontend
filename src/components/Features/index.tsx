edf190df8e77e1115b313b0d550d2142101ec64b
Ananth Prabhu T <prabhuanantht@gmail.com>
2024-10-17 22:59:13 +0530

    first commit

---
+++ src/components/Features/index.tsx
@@ -0,0 +1,26 @@
+import SectionTitle from "../Common/SectionTitle";
+import SingleFeature from "./SingleFeature";
+import featuresData from "./featuresData";
+
+const Features = () => {
+  return (
+    <>
+      <section id="features" className="py-16 md:py-20 lg:py-28">
+        <div className="container">
+          <SectionTitle
+            title="Main Features"
+            paragraph="Empowering innovation through intelligent models, each feature crafted to unlock new possibilities in the world of machine learning."
+            center
+          />
+          <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
+            {featuresData.map((feature) => (
+              <SingleFeature key={feature.id} feature={feature} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Features;
